b24b575f534f85d5d46321f53de1261d22db00ee
EdherUnal <101904222+EdherUnal@users.noreply.github.com>
2022-12-03 11:19:38 -0500

    LABKNN_CLAS
    LABKNN_CLAS

--- preguntas.py
+++ preguntas.py
@@ -60,14 +60,16 @@ def pregunta_02():
     from sklearn.preprocessing import OrdinalEncoder
 
     # Transforme las variables de entrada usando fit_transform
-    X = OrdinalEncoder().fit_transform(X)
+    OrdinalEncoder = OrdinalEncoder(categories="auto", dtype=np.float64,)
+    OrdinalEncoder.fit(X)
+    X = OrdinalEncoder.transform(X)
 
     # Importe KNeighborsClassifier de sklearn.neighbors
     from sklearn.neighbors import KNeighborsClassifier
     
 
     # Cree un un clasificador k-NN con 6 vecinos
-    knn = KNeighborsClassifier(n_neighbors=6)
+    knn = KNeighborsClassifier(n_neighbors=5)
 
     # Entrene el clasificador con el conjunto de entrenamiento
     knn.fit(X,y)
